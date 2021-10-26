578ff09571700e60ff5e77ae039ae3ba59a70892
eyou102 <eyou102@gmail.com>
2021-10-26 15:59:35 -0400

    working on slopes

--- bathygrid/algorithms.py
+++ bathygrid/algorithms.py
@@ -1,5 +1,7 @@
 import numba
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def np_grid_mean(depth: np.array, cell_indices: np.array, grid: np.ndarray, density_grid: np.ndarray, tvu: np.array = np.array([]),
@@ -125,6 +127,79 @@ def np_grid_shoalest(depth: np.array, cell_indices: np.array, grid: np.ndarray, 
         thu_grid[urow, ucol] = thu_shoalest
 
     return grid, tvu_grid, thu_grid
+
+
+def _calculate_slopes(x: np.array, y: np.array, z: np.array, cell_indices: np.array, grid_x: np.array, grid_y: np.array,
+                      visualize: bool = False):
+    """
+    Perform least squares regression to get plane equation of best fit plane for each grid cell.  grid cells are defined
+    by the provided (grid_x, grid_y) edge values.
+
+    Optionally visualize the least squares planes using matplotlib wireframe
+
+    Parameters
+    ----------
+    x
+        1d x values
+    y
+        1d y values
+    z
+        1d z values
+    cell_indices
+        1d index of which cell each point belongs to
+    grid_x
+        1d x values for the grid edges
+    grid_y
+        1d y values for the grid edges
+    visualize
+        if True, plots the points and planes
+
+    Returns
+    -------
+    np.ndarray
+        (m,n) grid of slopes in the x direction for each cell/plane, m = len(grid_x) - 1, n = len(grid_y) - 1
+    np.ndarray
+        (m,n) grid of slopes in the x direction for each cell/plane, m = len(grid_x) - 1, n = len(grid_y) - 1
+    """
+
+    x_slope_grid = np.zeros((grid_x.shape[0] - 1, grid_y.shape[0] - 1), dtype=np.float32)
+    y_slope_grid = np.zeros((grid_x.shape[0] - 1, grid_y.shape[0] - 1), dtype=np.float32)
+
+    if visualize:
+        plt.figure()
+        ax = plt.subplot(111, projection='3d')
+        ax.scatter(x, y, z, color='b')
+        lstsq_grid = np.full((grid_x.shape[0], grid_y.shape[0]), np.float32(np.nan), dtype=np.float32)
+        lstsq_x, lstsq_y = np.meshgrid(grid_x, grid_y)
+
+    cell_sort = np.argsort(cell_indices)
+    unique_indices, uidx, ucounts = np.unique(cell_indices[cell_sort], return_index=True, return_counts=True)
+    urow, ucol = np.unravel_index(unique_indices, x_slope_grid.shape)
+
+    z_split = np.split(z[cell_sort], uidx)[1:]
+    x_split = np.split(x[cell_sort], uidx)[1:]
+    y_split = np.split(y[cell_sort], uidx)[1:]
+
+    for row, col, x_cell, y_cell, z_cell in zip(urow, ucol, x_split, y_split, z_split):
+        # following equation of plane ax + by + c = z we set up the Ax = B matrices and do the least squares fit
+        a_data = np.column_stack([x_cell, y_cell, np.ones(x_cell.shape[0], dtype=np.float32)])
+        b_data = np.column_stack([z_cell])
+        fit, residual, rnk, s = np.linalg.lstsq(a_data, b_data, rcond=None)
+
+        minz = fit[0] * grid_x[row] + fit[1] * grid_y[row] + fit[2]
+        maxz_xdirect = fit[0] * grid_x[row + 1] + fit[1] * grid_y[row] + fit[2]
+        maxz_ydirect = fit[0] * grid_x[row] + fit[1] * grid_y[row + 1] + fit[2]
+        x_slope_grid[row, col] = (maxz_xdirect - minz) / (grid_x[row + 1] - grid_x[row])
+        y_slope_grid[row, col] = (maxz_ydirect - minz) / (grid_y[row + 1] - grid_y[row])
+        if visualize:
+            lstsq_grid[row, col] = fit[0] * lstsq_x[row, col] + fit[1] * lstsq_y[row, col] + fit[2]
+    if visualize:
+        ax.plot_wireframe(lstsq_x, lstsq_y, lstsq_grid, color='k')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        plt.show()
+    return x_slope_grid, y_slope_grid
 
 
 @numba.njit
